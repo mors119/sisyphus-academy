40979f18b1c344629d0647b3f10e386d580fe476
heesk0223@gmail.com <heesk0223@gmail.com>
2026-06-16 13:37:46 +0900

    fix: add mail mock bean

--- backend/src/test/java/com/sisyphus/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/sisyphus/backend/BackendApplicationTests.java
@@ -2,11 +2,16 @@ package com.sisyphus.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class BackendApplicationTests {
+
+    @MockitoBean
+    JavaMailSender mailSender;
 
     @Test
     void contextLoads() {
